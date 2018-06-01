23ce842972f8b4e07ae181a563c3d6e2ea705d70
Simon Hoeferlin <s@hoeferlins.de>
2018-06-01 10:16:32 +0200

    All

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'products#index' # And remove the generated route `products/index`
+  root to: 'products#index' # And remove the generated route `products/index
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
